941d9346bf84380eac405aaf5ebc1e2941def3a4
Alejandro Roa <alejodev04@gmail.com>
2020-09-18 09:47:36 -0400

    add shorting table, problem with shorting feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table'  
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { MatListModule } from '@angular/material/list';
     MainMenuComponent
   ],
   imports: [
+    MatTableModule,
     BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
